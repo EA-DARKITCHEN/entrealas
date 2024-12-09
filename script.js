6c0a44dbe2696032bf310e0cadd1d115c565621f
EA-DARKITCHEN <contacto.entrealas@gmail.com>
2024-12-09 16:13:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+window.addEventListener('load', function() {
+    // Oculta el loader
+    document.getElementById('loader').style.display = 'none';
+    // Muestra el contenido
+    document.getElementById('content').style.display = 'block';
+});
